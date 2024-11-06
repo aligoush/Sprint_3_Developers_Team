3145bc01e201006e218dbfe2726052e8742c09e5
paumaneja <paumaneja@gmail.com>
2024-11-06 13:16:48 +0100

    dbconnection

--- Escape_Room/src/main/java/dao/DBConnection.java
+++ Escape_Room/src/main/java/dao/DBConnection.java
@@ -1,18 +1,16 @@
 package dao;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
 
 public class DBConnection {
 
     protected Connection connection;
     protected ResultSet resultSet;
     protected PreparedStatement preparedStatement;
-    //private static final String URL = "jdbc:mysql://localhost";
+    private static final String URL = "jdbc:mysql://localhost";
     private static final String USER = "root";
     private String password;
 
@@ -28,8 +26,48 @@ public class DBConnection {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection(URL, USER, this.password);
-        } catch
+        } catch (ClassNotFoundException | SQLException e){
+            System.err.println("Error while connecting to the DB.");
+        }
         return connection;
+    }
+
+    public PreparedStatement getPreparedStatement(){
+        return preparedStatement;
+    }
+
+    public ResultSet getResultSet() {
+        return resultSet;
+    }
+
+    public void closeResultSet() {
+        try {
+            resultSet.close();
+        } catch (SQLException ex) {
+            System.err.println("Error. Couldn't close resultSet.");
+        }
+    }
+
+    public void closeStatement() {
+        try {
+            preparedStatement.close();
+        } catch (SQLException ex) {
+            System.err.println("Error. Couldn't close statement.");
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            if (resultSet != null) {
+                closeResultSet();
+            }
+            if (preparedStatement != null) {
+                closeStatement();
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Error. Couldn't close the connection properly.");
+        }
     }
 
     public static String readPassword() throws IOException {
